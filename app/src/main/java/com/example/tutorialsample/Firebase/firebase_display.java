a43ca81f0619bc6ee5db761432ba088d5aa18061
jovannitjuandi <jovanni.tjuandi@gmail.com>
2022-11-08 01:11:07 +1100

    Complete Firebase Authentication

--- app/src/main/java/com/example/tutorialsample/Firebase/firebase_display.java
+++ app/src/main/java/com/example/tutorialsample/Firebase/firebase_display.java
@@ -2,15 +2,47 @@ package com.example.tutorialsample.Firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tutorialsample.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class firebase_display extends AppCompatActivity {
+    private Button logout_button;
+    private TextView current_user_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_display);
+
+        current_user_email = findViewById(R.id.current_user_email);
+        current_user_email.setText(GetCurrentUserEmail());
+
+        logout_button = findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            BackToFirebaseHome();
+        });
+    }
+
+/**************************************************************************************************/
+/*                                         HELPER METHODS                                         */
+/**************************************************************************************************/
+
+    private void BackToFirebaseHome() {
+        Intent backHome = new Intent(firebase_display.this, firebase_login.class);
+        startActivity(backHome);
+    }
+
+    private String GetCurrentUserEmail() {
+        Intent intent = getIntent();
+        String currentUserEmail = intent.getStringExtra(getResources().getString(R.string.current_user_intent));
+
+        return currentUserEmail;
     }
 }
--- app/src/main/java/com/example/tutorialsample/Firebase/firebase_login.java
+++ app/src/main/java/com/example/tutorialsample/Firebase/firebase_login.java
@@ -1,10 +1,19 @@
 package com.example.tutorialsample.Firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tutorialsample.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -24,11 +33,125 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 public class firebase_login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView email_input, password_input, error_message;
+    Button authenticate_button, create_user_button;
+    private String TAG = "APP_GENERATED_Firebase_Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
+        error_message = findViewById(R.id.error_message);
+
+        authenticate_button = findViewById(R.id.authenticate_button);
+        authenticate_button.setOnClickListener(view -> {
+            error_message.setText("");
+            if (IsValidInput()) {
+                AuthenticateUser(GetEmail(), GetPassword());
+            }
+        });
+
+        create_user_button = findViewById(R.id.create_user_button);
+        create_user_button.setOnClickListener(view -> {
+            error_message.setText("");
+            if (IsValidInput()) {
+                CreateUser(GetEmail(), GetPassword());
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            GoToDisplay();
+        }
+    }
+
+
+    // FIREBASE HELPER METHODS
+    private String GetEmail() {
+        String email = email_input.getText().toString();
+        if (email == null || email.length() == 0) {
+            email = "";
+        }
+
+        return email;
+    }
+
+    private String GetPassword() {
+        String password = password_input.getText().toString();
+        if (password == null || password.length() == 0) {
+            password = "";
+        }
+
+        return password;
+    }
+
+    private boolean IsValidInput() {
+        String email = email_input.getText().toString();
+        if (email == null || email.length() == 0) {
+            error_message.setText("Email cannot be empty");
+            return false;
+        }
+
+        String password = password_input.getText().toString();
+        if (password == null || password.length() == 0) {
+            error_message.setText("Password cannot be empty");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean AuthenticateUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "SIGNIN SUCCESS!");
+                    GoToDisplay();
+                } else {
+                    String failCause = task.getException().getMessage();
+                    error_message.setText(failCause);
+                    Log.d(TAG, "SIGNIN - " + failCause);
+                }
+            }
+        });
+
+        return true;
+    }
+
+    private boolean CreateUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "SIGNUP SUCCESS!");
+                    GoToDisplay();
+                } else {
+                    String failCause = task.getException().getMessage();
+                    error_message.setText(failCause);
+                    Log.d(TAG, "SIGNUP FAILED - " + failCause);
+                }
+            }
+        });
+
+        return true;
+    }
+
+    private void GoToDisplay() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        Intent intent = new Intent(firebase_login.this, firebase_display.class);
+        intent.putExtra(getResources().getString(R.string.current_user_intent), currentUser.getEmail());
+        startActivity(intent);
     }
 }
